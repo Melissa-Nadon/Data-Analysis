fbe65d1a834d70ba51968ad11320efa1b0d810ee
Melissa-Nadon <64729626+Melissa-Nadon@users.noreply.github.com>
2024-08-07 13:13:18 +0700

    app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
+import re
 
 # Set page config with custom icon
 st.set_page_config(page_title="CSV Data Analysis", page_icon="logo.png")
@@ -61,12 +62,53 @@ st.markdown(
         background-color: #fff;
         color: #333;
     }
+
+    /* Change selected item background color to green */
+    .css-1n76uvr.e1fqkh3o3 {  /* Class for selected item */
+        background-color: #00FF00 !important; /* Green */
+        color: white !important;
+    }
+    .css-1n76uvr.e1fqkh3o3:hover {
+        background-color: #00CC00 !important; /* Darker Green */
+        color: white !important;
+    }
+    .css-1n76uvr.e1fqkh3o3:focus {
+        background-color: #00CC00 !important; /* Darker Green */
+        color: white !important;
+    }
+    .css-1n76uvr.e1fqkh3o3:active {
+        background-color: #009900 !important; /* Even Darker Green */
+        color: white !important;
+    }
+
+    /* Change the invalid input underline color */
+    .gdg-invalid .gdg-izpuzkl {
+        -webkit-text-decoration: underline;
+        text-decoration: underline;
+        text-decoration-color: #00FF00; /* Change to green */
+    }
+
     </style>
     """,
     unsafe_allow_html=True
 )
 
+
+
 st.title("CSV Data Analysis 📊")
+
+def sort_columns(cols):
+    # Extract the numerical part of the column names and sort based on that
+    def tryint(s):
+        try:
+            return int(s)
+        except ValueError:
+            return s
+
+    def alphanum_key(s):
+        return [tryint(c) for c in re.split('([0-9]+)', s)]
+
+    return sorted(cols, key= alphanum_key)
 
 # Upload CSV file
 csv_file = st.file_uploader("Upload your CSV file", type=["csv"])
@@ -92,7 +134,7 @@ if csv_file:
         if df.empty:
             st.error("The DataFrame is empty after processing the 'DateTime' column.")
         else:
-            tabs = st.tabs(["Data Analysis", "Count and Resample Analysis"])
+            tabs = st.tabs(["Data Analysis", "Count and Resample Analysis", "Period Analysis"])
 
             with tabs[0]:
                 show_data = st.checkbox("Show uploaded data")
@@ -105,8 +147,17 @@ if csv_file:
 
                 # Select columns for calculations
                 numeric_columns = [col for col in df.columns if pd.api.types.is_numeric_dtype(df[col])]
-                selected_columns = st.multiselect("Select columns to calculate", numeric_columns)
+                
+                select_all = st.checkbox("Select All Columns", key="select_all_data_analysis")
+                if select_all:
+                    selected_columns = st.multiselect("Select columns to calculate", numeric_columns, default=numeric_columns)
+                else:
+                    selected_columns = st.multiselect("Select columns to calculate", numeric_columns)
+
                 if selected_columns:
+                    # Sort columns
+                    selected_columns = sort_columns(selected_columns)
+
                     # Convert minute data to hourly if needed
                     df_hourly = df[selected_columns].resample('H').mean()
 
@@ -178,15 +229,31 @@ if csv_file:
                 st.write("Count and Resample Analysis")
 
                 # Select columns for count analysis
-                selected_columns_for_count = st.multiselect("Select columns to count", numeric_columns)
+                select_all_count = st.checkbox("Select All Columns for Count", key="select_all_count")
+                if select_all_count:
+                    selected_columns_for_count = st.multiselect("Select columns to count", numeric_columns, default=numeric_columns)
+                else:
+                    selected_columns_for_count = st.multiselect("Select columns to count", numeric_columns)
+
                 if selected_columns_for_count:
+                    # Sort columns
+                    selected_columns_for_count = sort_columns(selected_columns_for_count)
+
                     count_data = df[selected_columns_for_count].count()
                     st.write("Count of non-NA/null values for each column:")
                     st.write(count_data)
 
                 # Select columns for resample analysis
-                selected_columns_for_resample = st.multiselect("Select columns to resample", numeric_columns)
+                select_all_resample = st.checkbox("Select All Columns for Resample", key="select_all_resample")
+                if select_all_resample:
+                    selected_columns_for_resample = st.multiselect("Select columns to resample", numeric_columns, default=numeric_columns)
+                else:
+                    selected_columns_for_resample = st.multiselect("Select columns to resample", numeric_columns)
+                
                 if selected_columns_for_resample:
+                    # Sort columns
+                    selected_columns_for_resample = sort_columns(selected_columns_for_resample)
+
                     resample_period = st.selectbox("Select period for resample", ['hourly', 'daily', 'weekly', 'monthly'])
 
                     # Resample data based on the selected period
@@ -237,6 +304,95 @@ if csv_file:
                         )
 
                         st.plotly_chart(fig)
+
+            with tabs[2]:
+                st.write("Period Analysis")
+
+                # Select start and end date
+                start_date = st.date_input("Start Date", key="start_date")
+                end_date = st.date_input("End Date", key="end_date")
+
+                if start_date and end_date:
+                    filtered_df = df.loc[start_date:end_date]
+                    
+                    select_all_period = st.checkbox("Select All Columns for Period Analysis", key="select_all_period")
+                    if select_all_period:
+                        selected_columns_for_period = st.multiselect("Select columns for period analysis", numeric_columns, default=numeric_columns, key="columns_for_period")
+                    else:
+                        selected_columns_for_period = st.multiselect("Select columns for period analysis", numeric_columns, key="columns_for_period")
+
+                    if selected_columns_for_period:
+                        # Sort columns
+                        selected_columns_for_period = sort_columns(selected_columns_for_period)
+
+                        # Calculate metrics for the selected period
+                        metrics = filtered_df[selected_columns_for_period].agg(['count', 'max', 'mean', 'min', 'std'])
+
+                        # Transpose the metrics dataframe to get the desired format
+                        metrics = metrics.T
+
+                        # Reorder the columns
+                        metrics = metrics[['count', 'mean', 'std', 'max', 'min']]
+                        metrics.columns = ['Count', 'Average', 'Standard Deviation', 'Maximum', 'Minimum']
+
+                        metrics.index.name = 'Sensor No.'
+
+                        st.write(f"**Summary period ({start_date} to {end_date}):**")
+                        st.write(metrics.style.format("{:.2f}"))
+
+                        csv_period = metrics.to_csv(index=True)
+                        st.download_button(
+                            label="Download Period Analysis Data as CSV",
+                            data=csv_period,
+                            file_name='period_analysis_data.csv',
+                            mime='text/csv',
+                        )
+
+                    # Breakdown Analysis
+                    breakdown_period = st.selectbox("Select breakdown period", ['hourly', 'daily', 'weekly', 'monthly'], key="breakdown_period")
+
+                    metrics_list = ['Average', 'Maximum', 'Minimum', 'Standard Deviation', 'Count']
+                    selected_metrics = st.multiselect("Select metrics to display", metrics_list, default=metrics_list)
+
+                    if not filtered_df[selected_columns_for_period].empty:
+                        if breakdown_period == 'hourly':
+                            df_breakdown = filtered_df[selected_columns_for_period].resample('H').agg(['mean', 'std', 'max', 'min', 'count'])
+                        elif breakdown_period == 'daily':
+                            df_breakdown = filtered_df[selected_columns_for_period].resample('D').agg(['mean', 'std', 'max', 'min', 'count'])
+                        elif breakdown_period == 'weekly':
+                            df_breakdown = filtered_df[selected_columns_for_period].resample('W').agg(['mean', 'std', 'max', 'min', 'count'])
+                        elif breakdown_period == 'monthly':
+                            df_breakdown = filtered_df[selected_columns_for_period].resample('M').agg(['mean', 'std', 'max', 'min', 'count'])
+
+                        df_breakdown.columns = ['_'.join(col).strip() for col in df_breakdown.columns.values]
+
+                        # Rename columns for metrics
+                        df_breakdown.columns = [col.replace('mean', 'Average').replace('max', 'Maximum').replace('min', 'Minimum').replace('std', 'Standard Deviation').replace('count', 'Count') for col in df_breakdown.columns]
+
+                        breakdown_dfs = {}
+
+                        for metric in selected_metrics:
+                            metric_df = df_breakdown.filter(like=metric)
+                            metric_df.columns = [col.replace(f'_{metric}', '') for col in metric_df.columns]
+                            metric_df.index.name = 'Sensor No.'
+                            breakdown_dfs[metric] = metric_df.T
+
+                        st.write("**Breakdown Analysis:**")
+                        for metric, breakdown_df in breakdown_dfs.items():
+                            st.write(f"**{metric.capitalize()}**")
+                            # ฟอร์แมตข้อมูลเฉพาะคอลัมน์ที่เป็นประเภท float
+                            float_cols = breakdown_df.select_dtypes(include=['float64', 'float32']).columns
+                            st.write(breakdown_df.reset_index().rename(columns={'index': 'Sensor No.'}).style.format({col: "{:.2f}" for col in float_cols}))
+
+                            csv_breakdown = breakdown_df.to_csv(index=True)
+                            st.download_button(
+                                label=f"Download {metric.capitalize()} Data as CSV",
+                                data=csv_breakdown,
+                                file_name=f'{metric}_breakdown_data.csv',
+                                mime='text/csv',
+                            )
+                    else:
+                        st.write("No data available for the selected period and columns.")
 
     else:
         st.error("The CSV file does not contain a 'DateTime' column. Please upload a CSV file with a 'DateTime' column.")
